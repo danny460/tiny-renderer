94c9387385d263cb9c5e12532e68ac5ca1416dfa
Dannyzlh <Danielzlh62@gmail.com>
2017-03-25 16:46:37 +0800

    update wireframe

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 
 void test(TGAImage &image);
 void wireframe(Model *model, TGAImage &image);
-void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color);
-void triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, const TGAColor &color);
+void line(Vec2i p0, Vec2i p1, TGAImage &image, const TGAColor &color);
+void triangle(Vec2i p0, Vec2i p1, Vec2i p2, TGAImage &image, const TGAColor &color);
 int testIntersection(int x0, int y0, int x1, int y1, int yy);
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
@@ -16,12 +16,10 @@ const int height = 800;
 
 int main(){
     TGAImage image(width, height, TGAImage::RGB);
-    Model headModel = Model("./obj/african_head.obj");
-    // test(image);
-    wireframe(&headModel,image);
+    Model model = Model("../obj/african_head/african_head.obj");
+    wireframe(&model,image);
     image.flip_vertically();
-    image.write_tga_file("output.tga");
-    
+    image.write_tga_file("../out/output.tga");
     return 0;
 }
 
@@ -31,64 +29,67 @@ void wireframe(Model *model, TGAImage &image){
         for (int j=0; j<3; j++) { 
             Vec3f v0 = model->vert(face[j]); 
             Vec3f v1 = model->vert(face[(j+1)%3]); 
-            int x0 = (v0.x+1.)*width/2.; 
-            int y0 = (v0.y+1.)*height/2.; 
-            int x1 = (v1.x+1.)*width/2.; 
-            int y1 = (v1.y+1.)*height/2.; 
-            line(x0, y0, x1, y1, image, white); 
+            v0.x = (v0.x+1.)*width/2.; 
+            v0.y = (v0.y+1.)*height/2.; 
+            v1.x = (v1.x+1.)*width/2.; 
+            v1.y = (v1.y+1.)*height/2.;
+            Vec2i vi0 = Vec2i(v0.x , v0.y);
+            Vec2i vi1 = Vec2i(v1.x, v1.y);
+
+            line(vi0, vi1, image, white); 
         } 
     }
 }
 
 void test(TGAImage &image){
     /*triangle test*/
-    triangle(10, 70, 50, 160, 70, 80, image, red);
-    triangle(180, 50,  150, 1,   70, 180, image, white);
-    triangle(180, 150, 120, 160, 130, 180, image, green);
+    // triangle(10, 70, 50, 160, 70, 80, image, red);
+    // triangle(180, 50,  150, 1,   70, 180, image, white);
+    // triangle(180, 150, 120, 160, 130, 180, image, green);
 }
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color){
-    bool steep = std::abs(y1-y0) > std::abs(x1-x0);
+void line(Vec2i p0, Vec2i p1, TGAImage &image, const TGAColor &color){
+    bool steep = std::abs(p1.y - p0.y) > std::abs(p1.x - p0.x);
     if(steep){
-        std::swap(x0, y0);
-        std::swap(x1, y1);
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
     }
-    if(x0 > x1){
-        std::swap(x0, x1);
-        std::swap(y0, y1);
+    if(p0.x > p1.x){
+        std::swap(p0.x, p1.x);
+        std::swap(p0.y, p1.y);
     }
-    int y = y0;
-    float derror = (y1-y)/(float)(x1-x0);
-    for(int x = x0 ; x <= x1 ; x++){
+    int y = p0.y;
+    float derror = (p1.y - y)/(float)(p1.x-p0.x);
+    for(int x = p0.x ; x <= p1.x ; x++){
         if(steep){
-            image.set((int) (y + (x - x0) * derror), x, color);
+            image.set((int) (y + (x - p0.x) * derror), x, color);
         }else{
-            image.set(x, (int) (y + (x - x0) * derror), color);
+            image.set(x, (int) (y + (x - p0.x) * derror), color);
         }
     }
 }
 
-void triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, const TGAColor &color){
-    int top = std::max( std::max(y0, y1), y2);
-    int bot = std::min( std::min(y0, y1), y2);
-    for(int y = top ; y >= bot; y--){
-        int x01, x12, x20;
-        x01 = testIntersection(x0, y0, x1, y1, y);
-        x12 = testIntersection(x1, y1, x2, y2, y);
-        x20 = testIntersection(x2, y2, x0, y0, y);
-        if(x01 == -1)
-            line(x12, y, x20, y, image, color);
-        else if( x12 == -1)    
-            line(x01, y, x20, y, image, color);   
-        else if( x20 == -1)   
-            line(x01, y, x12, y, image, color);
-        else if(x01 == x12 || x01 == x20) line(x12, y, x20, y, image, color);
-        else if (x12 == x20) line(x01, y, x20, y, image, color);         
-    }
-    line(x0, y0, x1, y1, image, color);
-    line(x1, y1, x2, y2, image, color);
-    line(x2, y2, x0, y0, image, color);
-}
+// void triangle(Vec2i p0, Vec2i p1, Vec2 p2, TGAImage &image, const TGAColor &color){
+//     int top = std::max( std::max(y0, y1), y2);
+//     int bot = std::min( std::min(y0, y1), y2);
+//     for(int y = top ; y >= bot; y--){
+//         int x01, x12, x20;
+//         x01 = testIntersection(x0, y0, x1, y1, y);
+//         x12 = testIntersection(x1, y1, x2, y2, y);
+//         x20 = testIntersection(x2, y2, x0, y0, y);
+//         if(x01 == -1)
+//             line(x12, y, x20, y, image, color);
+//         else if( x12 == -1)    
+//             line(x01, y, x20, y, image, color);   
+//         else if( x20 == -1)   
+//             line(x01, y, x12, y, image, color);
+//         else if(x01 == x12 || x01 == x20) line(x12, y, x20, y, image, color);
+//         else if (x12 == x20) line(x01, y, x20, y, image, color);         
+//     }
+//     line(x0, y0, x1, y1, image, color);
+//     line(x1, y1, x2, y2, image, color);
+//     line(x2, y2, x0, y0, image, color);
+// }
 
 int testIntersection(int x0, int y0, int x1, int y1, int yy){
     int top = std::max(y0, y1);
