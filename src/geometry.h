4f79c78f282ac15f86b6cc8de3ace9666013f2d1
zhang linghan <zhang.linghan62@gmail.com>
2017-03-30 16:06:28 +0800

    extend vector

--- src/geometry.h
+++ src/geometry.h
@@ -6,16 +6,14 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 template <class t> struct Vec2 {
-	union {
-		struct {t u, v;};
-		struct {t x, y;};
-		t raw[2];
-	};
-	Vec2() : u(0), v(0) {}
-	Vec2(t _u, t _v) : u(_u),v(_v) {}
-	inline Vec2<t> operator +(const Vec2<t> &V) const { return Vec2<t>(u+V.u, v+V.v); }
-	inline Vec2<t> operator -(const Vec2<t> &V) const { return Vec2<t>(u-V.u, v-V.v); }
-	inline Vec2<t> operator *(float f)          const { return Vec2<t>(u*f, v*f); }
+	t x, y;
+	Vec2<t>() : x(t()), y(t()) {}
+    Vec2<t>(t _x, t _y) : x(_x), y(_y) {}
+    Vec2<t>(const Vec2<t> &v) : x(t()), y(t()) { *this = v; }
+	inline t& 	   operator [](const int i){ if(i <= 0) return x; else return y;}
+	inline Vec2<t> operator +(const Vec2<t> &V) const { return Vec2<t>(x+V.x, y+V.y); }
+	inline Vec2<t> operator -(const Vec2<t> &V) const { return Vec2<t>(x-V.x, y-V.y); }
+	inline Vec2<t> operator *(float f)          const { return Vec2<t>(x*f, y*f); }
 	inline float dot(Vec2<t> &v){ return x * v.x + y * v.y; }
 	inline float dot(Vec2<t> &v0, Vec2<t> &v1){ return v0.x * v1.x + v0.y * v1.y;}
 
@@ -23,20 +21,18 @@ template <class t> struct Vec2 {
 };
 
 template <class t> struct Vec3 {
-	union {
-		struct {t x, y, z;};
-		struct { t ivert, iuv, inorm; };
-		t raw[3];
-	};
+	t x,y,z;
 	Vec3() : x(0), y(0), z(0) {}
 	Vec3(t _x, t _y, t _z) : x(_x),y(_y),z(_z) {}
+	inline t& 	   operator [](const int i){ if(i <= 0) return x; else if(i == 1) return y; else return z;}
 	inline Vec3<t> operator ^(const Vec3<t> &v) const { return Vec3<t>(y*v.z-z*v.y, z*v.x-x*v.z, x*v.y-y*v.x); } //cross product
 	inline Vec3<t> operator +(const Vec3<t> &v) const { return Vec3<t>(x+v.x, y+v.y, z+v.z); }
 	inline Vec3<t> operator -(const Vec3<t> &v) const { return Vec3<t>(x-v.x, y-v.y, z-v.z); }
 	inline Vec3<t> operator *(float f)          const { return Vec3<t>(x*f, y*f, z*f); }
-	inline t       operator *(const Vec3<t> &v) const { return x*v.x + y*v.y + z*v.z; }
-	inline float dot(const Vec3<t> &v) const { return x*v.x + y*v.y + z*v.z; }
-	inline Vec3<t> cross(const Vec3<t> &v) const { return Vec3<t>( y*v.z - z*v.y, z*v.x-x*v.z, x*v.y-y*v.x ); }
+	inline t	   operator *(const Vec3<t> &v) const { return x*v.x + y*v.y + z*v.z; }
+	inline float   dot(const Vec3<t> &v) 		const { return x*v.x + y*v.y + z*v.z; }
+	inline Vec3<t> cross(const Vec3<t> &v) 		const { return Vec3<t>( y*v.z - z*v.y, z*v.x-x*v.z, x*v.y-y*v.x ); }
+
 	float norm () const { return std::sqrt(x*x+y*y+z*z); }
 	Vec3<t> & normalize(t l=1) { *this = (*this)*(l/norm()); return *this; }
 	template <class > friend std::ostream& operator<<(std::ostream& s, Vec3<t>& v);
